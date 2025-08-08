551e2426123399d98005895daa9fc2be372a03c8
clebermedina <clebermedina@gmail.com>
2025-08-08 17:48:33 -0300

    Versão inicial do site

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Suaviza a rolagem para âncoras na página inicial
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetElement = document.querySelector(this.getAttribute('href'));
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+
+});
